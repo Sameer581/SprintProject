573f2d1d4410729289b8b7aa2abb50957c0c2c37
Rishib#321 <rishitab2209@gmail.com>
2026-04-27 20:50:56 +0530

    friendship CRUD

--- src/main/java/com/cg/service/FriendshipService.java
+++ src/main/java/com/cg/service/FriendshipService.java
@@ -9,41 +9,31 @@ public interface FriendshipService {
 	
 	// total 10 end points
 	
-	
 	// Sending friend request
 	 FriendshipDto sendFriendRequest(FriendshipDto dto);                  
-	 
 	 
 	 // accepting friend request
 	 FriendshipDto acceptFriendRequest(Long friendshipId);                
 	 
-	 
 	 // rejecting friend request
 	 void rejectFriendRequest(Long friendshipId);
-	 
 	 
 	 // get all pending requests from users
 	 List<FriendshipDto> getPendingRequests(Long userId);
 	 
-	 
 	 // get friendship by friendshipId
 	 FriendshipDto getFriendshipById(Long friendshipId);
-	 
 	 
 	 // friends by user id
 	 List<FriendshipDto> getFriendsByUserId(Long userId);  
 	              
-	    
 	 // finding list of all friendships
 	 List<FriendshipDto> getAllFriendships();                            
 	    
-	
 	 FriendshipDto checkFriendship(Integer userId1, Integer userId2);   
 	    
-	 
 	 // get number of friends count
 	 Integer getFriendsCount(Integer userId);                           
-	    
 
 	 // deleting/removing friend
 	 // void removeFriend(Integer friendshipId);
