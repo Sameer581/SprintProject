817403d1fffcf38835082ff44d5b5017e8d34c7d
Rishib#321 <rishitab2209@gmail.com>
2026-04-27 12:33:53 +0530

    add friendship repo

--- src/main/java/com/cg/dto/UpdateFriendshipDto.java
+++ src/main/java/com/cg/dto/UpdateFriendshipDto.java
@@ -3,7 +3,7 @@ package com.cg.dto;
 import com.cg.enums.FriendshipStatus;
 
 //UPDATE DTO - for accepting or rejecting a friend request
-public class UpdateFriendshipDTO {
+public class UpdateFriendshipDto {
 	
 	
  private FriendshipStatus status;     // PENDING → ACCEPTED
--- src/main/java/com/cg/repository/FriendshipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.entity.Friendship;
-
-public interface FriendshipRepository extends JpaRepository<Friendship, Integer>{
-
-}
